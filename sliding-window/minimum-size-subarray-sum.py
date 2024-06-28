754b46e02781eb863c31ca1c0cb0408acc532c10
aniket mishrikotkar <71699313+aniket-mish@users.noreply.github.com>
2024-06-28 15:29:05 +0530

    Create minimum-size-subarray-sum.py

---
+++ sliding-window/minimum-size-subarray-sum.py
@@ -0,0 +1,33 @@
+# O(n) time | O(n) space
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+        Variable length sliding window.
+
+        We find the min subarray thats >= target but then when this problem is assigned to subordinate,
+        he adds his number into the window and the shrinks the subarray till he gets the optimal one.
+        """
+        global_answer = float("inf")
+        left = 0
+        window_sum = 0
+
+        # Iterate over the array
+        for i in range(len(nums)):
+
+            # Work to be done by subarrays ending at index i
+
+            # Add the number at index i
+            window_sum += nums[i]
+
+            while left <= i and window_sum >= target:
+                window_sum -= nums[left]
+                global_answer = min(global_answer, i-left+1) # length of min subarray with target as sum
+                left += 1
+
+        if global_answer == float("inf"):
+            return 0
+
+        return global_answer
+
+
+
