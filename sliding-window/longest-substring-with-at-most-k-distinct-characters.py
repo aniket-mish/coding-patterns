9838fbc422ad0477bbf62b15447bb5a8e54e7e5d
aniket mishrikotkar <71699313+aniket-mish@users.noreply.github.com>
2024-07-07 20:04:26 +0530

    Add a new problem
    Similar problems

---
+++ sliding-window/longest-substring-with-at-most-k-distinct-characters.py
@@ -0,0 +1,27 @@
+# O(n) time | O(1) space
+def longest_substring_with_at_most_k_distinct_characters(s: str, k: int):
+  
+  left = 0
+  hmap = {}
+  global_max = 0
+
+  for i in range(len(s)):
+    # Max length substring ending at index i
+    # and containing at most k distinct characters
+
+    if s[i] in hmap:
+      hmap[s[i]] += 1
+    else:
+      hmap[s[i]] = 1
+
+    while left <= i and len(hmap) > k:
+      hmap[s[left]] -= 1
+
+      if hmap[s[left]] == 0:
+        del hmap[s[left]]
+
+      left += 1
+
+    global_max = max(global_max, i-left+1)
+
+  return global_max
