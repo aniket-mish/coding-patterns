ed9273ac65ac337ffe2c46e7ab5d1ba3dc454db3
aniket mishrikotkar <71699313+aniket-mish@users.noreply.github.com>
2024-07-07 16:34:50 +0530

    Add logic for the problem

--- sliding-window/max-consecutive-one-3.py
+++ sliding-window/max-consecutive-one-3.py
@@ -1,1 +1,27 @@
+# O(n) time | O(1) space
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        """
+        Variable length sliding window.
+        """
+        
+        left = 0
+        window_zeros = 0
+        global_max = 0
 
+        for i in range(len(nums)):
+            # Work to be done to solve local problem ending at index i
+
+            # Add nums[i] to the window
+            if nums[i] == 0:
+                window_zeros += 1
+
+            while left <= i and window_zeros > k:
+                if nums[left] == 0:
+                    window_zeros -= 1
+                left += 1
+
+            if i-left+1 > global_max:
+                global_max = i-left+1
+
+        return global_max
