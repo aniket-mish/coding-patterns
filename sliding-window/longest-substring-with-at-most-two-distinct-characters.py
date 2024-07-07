2f94fb8f6177dc84d1257fd84d7cac9a9eb98513
aniket mishrikotkar <71699313+aniket-mish@users.noreply.github.com>
2024-07-07 19:54:28 +0530

    Add logic
    This is similar to the fruits-into-basket problem

---
+++ sliding-window/longest-substring-with-at-most-two-distinct-characters.py
@@ -0,0 +1,27 @@
+# O(n) time | O(1) space
+def longest_substring_with_at_most_two_distinct_characters(s: str):
+  
+  left = 0
+  hmap = {}
+  global_max = 0
+
+  for i in range(len(s)):
+    # Max length substring ending at index i
+    # and containing at most 2 distinct characters
+
+    if hmap[s[i]] in hmap:
+      hmap[s[i]] += 1
+    else:
+      hmap[s[i]] = 1
+
+    while left <= i and len(hmap) > 2:
+      hmap[s[left]] -= 1
+
+      if hmap[s[left]] == 0:
+        del hmap[s[left]]
+
+      left += 1
+
+    global_max = max(global_max, i-left+1)
+
+  return global_max
