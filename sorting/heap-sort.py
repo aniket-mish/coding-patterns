3947779772c253ea893d342f1392159a1ac0505d
aniket mishrikotkar <71699313+aniket-mish@users.noreply.github.com>
2024-07-12 17:49:17 +0530

    Update heap-sort.py

--- sorting/heap-sort.py
+++ sorting/heap-sort.py
@@ -1,1 +1,12 @@
+# We need to use the heapq library
+# O(nlogn) time
+def heap_sort(arr):
+  # Build a minheap on the array
+  heapq.heapify(arr)
 
+  # Keep popping the successive elements from the minheap until it is empty
+  result = []
+  while len(arr) > 0:
+    result.append(heapq.heappop(arr))
+
+  return result
